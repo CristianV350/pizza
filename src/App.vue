9232b7759f1f18038bd41970ff3640bbe74bf7d1
Cristian <cvicol2@gmail.com>
2023-06-13 19:39:09 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header-component />
+    <main>
+      <router-view />
+    </main>
+    <footer-component />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
+export default {
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+};
+</script>
